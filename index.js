f3ee54a1abae4cd1103580e7bba7fe90ac60470b
natashafir <flyelin@gmail.com>
2021-02-22 00:07:17 +0200

    Add name of user

--- index.js
+++ index.js
@@ -36,17 +36,35 @@ function allUsers (array) {
     // console.log(friends);
 }
 
-function createCardItem (item) {
+// function createCardItem (item) {
+//
+//     const img = document.createElement('img');
+//     CARDS.appendChild(img);
+//     img.classList.add('card-wrapper');
+//     console.log(CARDS);
+//     img.src = item.photo;
+// }
 
+
+function createCardItem (item) {
+    const cardWrapper = document.createElement('div');
+    cardWrapper.classList.add('card-wrapper');
+    CARDS.appendChild(cardWrapper);
     const img = document.createElement('img');
-    CARDS.appendChild(img);
+    cardWrapper.appendChild(img);
+    img.classList.add('img-card');
+    const name = document.createElement('p');
+    cardWrapper.appendChild(name);
+    const userName = document.createTextNode(item.name);
+    name.appendChild(userName);
     console.log(CARDS);
+    console.log(item.name);
     img.src = item.photo;
 }
 
 function renderUsers() {
     friends.forEach(elem => {
-        console.log(elem);
+        // console.log(elem);
         // CARDS.appendChild(createCardItem(elem));
         createCardItem(elem)
     });
