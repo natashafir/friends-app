dd6923b0af68208dd4b7c2fb8aa3b982655a2a47
natashafir <flyelin@gmail.com>
2021-02-23 01:14:27 +0200

    Add users info

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function initialApp() {
         .then(response => response.json())
         // .then(data => {allUsers = data.results;
         .then((json) => {
-            allUsers(json.results);
+            userTemplate(json.results);
         });
     // CARDS.appendChild(img);
     // return allUsers.map(users => console.log(users.picture.large));
@@ -18,22 +18,16 @@ function initialApp() {
     // } );
 }
 
-function allUsers (array) {
+function userTemplate (array) {
     friends = array.map(user => {
         return {
             name: `${user.name.first} ${user.name.last}`,
             age: user.dob.age,
-            phone: user.cell,
-            email: user.email,
             gender: user.gender,
             photo: user.picture.large
-
         }
-
-
     });
     renderUsers();
-    // console.log(friends);
 }
 
 // function createCardItem (item) {
@@ -45,8 +39,8 @@ function allUsers (array) {
 //     img.src = item.photo;
 // }
 
-
 function createCardItem (item) {
+    // console.log(item);
     const cardWrapper = document.createElement('div');
     cardWrapper.classList.add('card-wrapper');
     CARDS.appendChild(cardWrapper);
@@ -57,21 +51,31 @@ function createCardItem (item) {
     cardWrapper.appendChild(name);
     const userName = document.createTextNode(item.name);
     name.appendChild(userName);
-    console.log(CARDS);
-    console.log(item.name);
+    const age = document.createElement('p');
+    cardWrapper.appendChild(age);
+    const userAge = document.createTextNode(item.age);
+    name.appendChild(userAge);
+    const gender = document.createElement('p');
+    cardWrapper.appendChild(gender);
+    const userGender = document.createTextNode(item.gender);
+    name.appendChild(userGender);
     img.src = item.photo;
-}
 
+}
 function renderUsers() {
     friends.forEach(elem => {
         // console.log(elem);
         // CARDS.appendChild(createCardItem(elem));
-        createCardItem(elem)
+        createCardItem(elem);
+        // friends.push(elem);
+
     });
+    console.log(friends)
 }
 
-
-
+// function filtrByGender() {
+//
+// }
 
 // console.log(allUsers);
 // console.log(users);
